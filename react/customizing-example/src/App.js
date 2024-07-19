fa6f83f247d1faf5ec1af8d7e14b7884ac207da1
erdnal <erdogannaldemirci@gmail.com>
2024-07-19 14:09:16 +0300

    metadd

---
+++ react/customizing-example/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Nav from './components/Nav';
+import Promo from './components/Promo';
+import Intro3 from './components/Intro3';
+import Intro2 from './components/Intro2';
+import Intro1 from './components/Intro1';
+import Footer from './components/Footer';
+import './App.css';
+
+    function Example() {
+      return ( <><><div><Nav /></div><div><Promo /></div></><div><Intro1 /></div><div><Intro2 /></div><div><Intro3 /></div><div><Footer /></div></>
+            
+            )
+    }
+    export default Example
